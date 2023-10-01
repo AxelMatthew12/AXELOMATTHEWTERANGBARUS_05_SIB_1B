89117f4ba25ea240ab93c65337bfb37113b75f5b
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-10-01 13:39:21 +0700

    Jobsheet 4 "Percobaan1 , No3"

--- Lingkaran05.java
+++ Lingkaran05.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-public class LingkaranNoabsen5 {
+public class Lingkaran05 {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
+        Scanner input= new Scanner(System.in);
+
+        //Rumus lingkaran 
         int r;
-        double keliling, luas, Phi=3.14;
+        double keliling, luas;
         System.out.println("Masukkan jari-jari lingkaran");
         r=input.nextInt();
-        keliling=2*3.14*r;
-        luas=Phi*r*r;
+        keliling=2*3.14*r; 
+        luas=3.14*r*r;
         System.out.println(keliling);
         System.out.println(luas);
 
