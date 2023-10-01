0078e12e14151335a594ff4709f863fa87e88b92
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-10-01 18:19:55 +0700

    Jobsheet 5 "Percobaan1 ,No1"

---
+++ PemilihanPercobaan1No5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PemilihanPercobaan1No5{
+    public static void main(String[] args) {
+        Scanner input00= new Scanner(System.in);
+
+        //menginput nilai
+        System.out.println("Masukkan angka:  ");
+        int angka = input00.nextInt();
+
+        //Menghitung nilai
+        String hasil;
+        hasil = (angka % 2 == 0) ? "Bilangan genap" :"Bilangan ganjil";
+        System.out.println(angka + "===adalah===" +hasil);
+}
+
+}
+
+    
