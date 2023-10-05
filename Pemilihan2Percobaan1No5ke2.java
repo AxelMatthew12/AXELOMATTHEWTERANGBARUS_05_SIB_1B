36d74f78cc5b015520becaf0f8285afcab6c6687
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-10-05 13:35:31 +0700

    Jobsheet 6 "Percobaan1, No4"

---
+++ Pemilihan2Percobaan1No5ke2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1No5ke2 {
+     public static void main(String[] args) {
+
+        //masukkan
+        Scanner input00= new Scanner(System.in);
+        System.out.println("===SELAMAT DATANG===");
+        System.out.println("Masukkan Tahun:     ");
+        int tahun = input00.nextInt();
+
+        System.out.println("Tahun adalah :  "+tahun);
+        if((tahun % 4)== 0) {
+            if((tahun % 100)==0){
+                
+                
+            }
+            System.out.println("Tahun Kabisat");
+        } else
+        System.out.println("Bukan Tahun Kabisat");
+    
+}
+}
