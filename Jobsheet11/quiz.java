36ecaf1e6d0ed29036ccf495b9c0463027139691
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-11-09 23:49:49 +0700

    quizAxel

---
+++ Jobsheet11/quiz.java
@@ -0,0 +1,38 @@
+package Jobsheet11;
+import java.util.Scanner;
+import java.util.Random;
+
+public class quiz {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        char menu = 'y';
+        do {
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+
+                if (answer == number) {
+                    success = true;
+                    System.out.println("Anda benar! Angka yang benar adalah " + number);
+                } else if (answer < number) {
+                    System.out.println("Tebakan terlalu kecil. Coba lagi.");
+                } else {
+                    System.out.println("Tebakan terlalu besar. Coba lagi.");
+                }
+
+            } while (!success);
+
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/t)? ");
+            menu = input.nextLine().charAt(0);
+
+        } while (menu == 't' || menu == 'T');
+
+        input.close();
+    }
+}
