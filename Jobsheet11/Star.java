47c03d8d19bd8db20ba7306b355c1d75b0b04040
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-11-09 23:48:54 +0700

    StarAxel

---
+++ Jobsheet11/Star.java
@@ -0,0 +1,17 @@
+package Jobsheet11;
+import java.util.Scanner;
+public class Star {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Masukkan nilai n : ");
+        int N = scan.nextInt();
+
+        for(int i=1;i<=N;i--){
+            System.out.print("*");
+        }
+
+       
+    }
+
+    
+}
