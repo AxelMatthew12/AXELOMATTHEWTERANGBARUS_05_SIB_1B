88d1b417bc0a7572807aed70186db2ed2174d7bb
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-11-30 12:47:12 +0700

    perubahan

---
+++ Jobsheet5/Lingkaran05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Lingkaran05 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        //Rumus lingkaran 
+        int r;
+        double keliling, luas;
+        System.out.println("Masukkan jari-jari lingkaran");
+        r=input.nextInt();
+        keliling=2*3.14*r; 
+        luas=3.14*r*r;
+        System.out.println(keliling);
+        System.out.println(luas);
+
+    }
+    
+}
